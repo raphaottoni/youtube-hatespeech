5fc9b17c9b7c0b517ed2d4d8fbb9afdc387d196e
Raphael Ottoni <rapha@Luthien.local>
2018-07-31 08:46:13 -0300

    forgot to upload collect_captions.py

---
+++ crawling/youtube/collect_captions.py
@@ -0,0 +1,42 @@
+import csv
+from youtube_api import YoutubeApi
+from tqdm import tqdm
+import json
+
+def main():
+
+    print("loading YouTube video IDs ...")
+    # Create the dictionary of medias
+    video_info = {}
+    with open('../../data/manually-filtered/youtube_videos.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None)  # skip the headers
+        for row in reader:
+            video_info[row[0]] = {  "media": row[1], "bias": row[2], "channel": row[3]}
+ 
+    
+    # collect all medias
+    all_videos = video_info.keys()
+    print("Done!")
+    
+    api = YoutubeApi()
+
+#    from IPython import embed
+#    embed()
+
+    # write json file with all data from the video, each line is a json
+    with open("../../data/videos.jsonl", 'w') as json_file:
+
+        print("Collecting captions from YouTube videos...")
+        for video in tqdm(all_videos):
+            print(video)
+            captions = api.collect_caption(video, "en", "both")
+            print(captions)
+            if captions:
+                video = { "videoID": video, "media": video_info[video]["media"], "bias": video_info[video]["bias"],  "channel": video_info[video]["channel"], "captions": captions} 
+                json.dump(video, json_file)
+        print("Done! Results written in ../../data/videos.jsonl")
+
+
+if __name__ == "__main__":
+    main()
